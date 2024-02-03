e0dfad9fa548a2a7977d5f7ffd83a2c08124d852
Devi-CodingTime <devipurnima26899@gmail.com>
2024-02-03 18:06:23 +0530

    nvhg

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 const fs = require("fs/promises");
 const fileName = "myfile.txt";
 const reader = async (fileName) => {
-  // write code here
-	// dont change function name
-  
+  await fs.readFile(fileName);
 };
-
-
+reader(fileName);
 module.exports =  reader ;
+
